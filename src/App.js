b06b28c41ee6e4e142b68b2072af8ebf51532110
Aphenphos <MaxwillWinters@gmail.com>
2022-09-26 18:33:38 -0400

    navbar works with active styling changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Main from './components/Main/Main';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
 import NotFound from './components/NotFound';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <main>
       <BrowserRouter>
+        <div id="navbar">
+          <NavLink to="/255/255/255">WHITE</NavLink>
+          <NavLink to="/100/255/100">GREEN</NavLink>
+          <NavLink to="/100/100/255">BLUE</NavLink>
+          <NavLink to="/255/100/100">RED</NavLink>
+        </div>
         <Switch>
+
           <Route path="/:r/:g/:b">
             <Main></Main>
           </Route>
